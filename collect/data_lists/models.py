e403321d720b6b3450f4f4e52ec41a257340eb8b
jobino <you@example.com>
2018-08-28 00:01:06 -0300

    models created

--- collect/data_lists/models.py
+++ collect/data_lists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    Processos = models.CharField(max_length=200)
+    courts = (
+        ('MT', 'TJSP'),
+        ('SP', 'TJMS'),
+    )
+
+    Tribunais = models.CharField(max_length=5, choices=courts, default='RED')
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.Processos
