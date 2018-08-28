e8c9aa2e6cb24e4e0bef9ec4114a30c6a71bb09a
jobino <you@example.com>
2018-08-28 00:01:24 -0300

    views modified

--- collect/data_lists/views.py
+++ collect/data_lists/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .forms import DataForm
+
 
 # Create your views here.
 
 def data_view(request):
-    data = ["bruno","bruno1","bruno2","bruno3"]
-    return render(request, 'view_datas/view.html', {'datas': data})
+    data_form = DataForm()
+    print(data_form)
+    return render(request, 'view_datas/view.html', {'datas': data_form})
