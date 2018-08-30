92361ce57e91e60e70c35f810d9360b9256862ce
jobino <you@example.com>
2018-08-30 01:57:16 -0300

    add method post

--- collect/data_lists/views.py
+++ collect/data_lists/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .forms import DataForm
+from .crawler.web_crawler import *
 
 
 # Create your views here.
 
 def data_view(request):
-    data_form = DataForm()
-    print(data_form)
-    return render(request, 'view_datas/view.html', {'datas': data_form})
+  if request.method == 'POST':
+    data_form = DataForm(request.POST)
+    if data_form.is_valid():
+      teste = data_form.cleaned_data['processes']
+      it = get_crawler_list()
+      print("---- --- ",it)
+  else:
+    data_form = DataForm()  # An unbound form
+  return render(request, 'view_datas/view.html', {'datas': data_form})
+
