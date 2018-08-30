babbd372de757e94d118d0baf97bea3b4c29a4e6
jobino <jobino-araujo@hotmail.com>
2018-08-30 17:52:58 -0300

    implementation web crawler

--- collect/data_lists/crawler/web_crawler.py
+++ collect/data_lists/crawler/web_crawler.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib3
-import re
-
+from more_itertools import unique_everseen 
 
 def crawler_data(link):
   page_formated = get_url(link)
@@ -11,6 +10,8 @@ def crawler_data(link):
     for span_data in span_datas:
       span_data_values = span_data.find_all("span", {"class": ""})
       list_datas = verify_space(span_data_values)
+  spanArea = get_value_tag(span_data,"span","labelClass",'Área: ')
+  list_datas.append(spanArea)
   return list_datas
 
 def get_url(urlSite):
@@ -25,16 +26,27 @@ def verify_space(data_value):
   for span_data_value in data_value:
     if(span_data_value.text[0:3].isspace() != True):
       list_datas.append(span_data_value.text.strip())
+  list_datas = list(unique_everseen(list_datas))
   return list_datas
 
-def remove_duplicate(duplicate):
-  final_list = []
-  for num in duplicate:
-    if num not in final_list:
-      final_list.append(num)
-  return final_list
+def get_crawler_list(url):
+  datas = crawler_data(url)
+  datas = set_dict(datas)
+  return datas
 
-def get_crawler_list():
-  datas = crawler_data('https://www.tjms.jus.br/cpopg5/show.do?processo.codigo=01001ZB2W0000&processo.foro=1&uuidCaptcha=sajcaptcha_d1c087a198884723ab35d670baaf13f4')
-  data_dict = remove_duplicate(datas)
+def crawler_concatenate_link(first_link, second_link, third_link, processes):
+  tjms_link_full = first_link + processes[:-5] + second_link + processes + third_link
+  return_list = get_crawler_list(tjms_link_full)
+  return return_list
+
+def set_dict(data_list):
+  data_dict = dict(zip(['Classe','Assunto','Distribuicao','Descricao','Controle','Juiz','Valor da Acao','Área'], data_list))
   return data_dict
+
+def get_value_tag(html, tag, value_class, specific_content):
+  data_values = html.find(tag, {"class": value_class})
+  data_values = data_values.parent.text.strip()
+  data_values = data_values.replace(specific_content, '')
+  return data_values
+
+print(crawler_concatenate_link("https://www.tjms.jus.br/cpopg5/search.do;jsessionid=F8E5227C1A100152C58411A02113AB4B.cpopg1?conversationId=&dadosConsulta.localPesquisa.cdLocal=1&cbPesquisa=NUMPROC&dadosConsulta.tipoNuProcesso=UNIFICADO&numeroDigitoAnoUnificado=", "&foroNumeroUnificado=0001&dadosConsulta.valorConsultaNuUnificado=", "&dadosConsulta.valorConsulta=&uuidCaptcha=sajcaptcha_d1c087a198884723ab35d670baaf13f4", "0821901-51.2018.8.12.0001"))
--- collect/data_lists/views.py
+++ collect/data_lists/views.py
@@ -6,13 +6,16 @@ from .crawler.web_crawler import *
 # Create your views here.
 
 def data_view(request):
+  final_data = dict()
   if request.method == 'POST':
     data_form = DataForm(request.POST)
     if data_form.is_valid():
       teste = data_form.cleaned_data['processes']
-      it = get_crawler_list()
-      print("---- --- ",it)
+      final_data = get_web_crawler()
+      print("---- --- ",final_data)
   else:
     data_form = DataForm()  # An unbound form
-  return render(request, 'view_datas/view.html', {'datas': data_form})
+  return render(request, 'view_datas/view.html', {'form': data_form, 'final_data':final_data})
 
+def get_web_crawler():
+  return crawler_concatenate_link("https://www.tjms.jus.br/cpopg5/search.do;jsessionid=F8E5227C1A100152C58411A02113AB4B.cpopg1?conversationId=&dadosConsulta.localPesquisa.cdLocal=1&cbPesquisa=NUMPROC&dadosConsulta.tipoNuProcesso=UNIFICADO&numeroDigitoAnoUnificado=", "&foroNumeroUnificado=0001&dadosConsulta.valorConsultaNuUnificado=", "&dadosConsulta.valorConsulta=&uuidCaptcha=sajcaptcha_d1c087a198884723ab35d670baaf13f4", "0821901-51.2018.8.12.0001")
